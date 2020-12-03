850b034ed3266ff74cb0a9562afce12c1f9ea688
rubin <rubinawale10@gmail.com>
2020-12-03 12:47:02 +0545

    Latest File 3

--- modules/admin.js
+++ modules/admin.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var timestamps = require('mongoose-timestamp');
 
-mongoose.connect('mongodb+srv://mongodb:rubin123123@cluster0.mcxac.mongodb.net/kitabharu?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://rubin123123:rubin123123@cluster0.mcxac.mongodb.net/kitabharu?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true});
 var conn = mongoose.connection;
 var Schema = mongoose.Schema; 
 
